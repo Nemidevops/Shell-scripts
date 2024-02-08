791f680fe430fd9e6facd8ff7ce87422c752dec0
nemidevops <nemivijay43@gmail.com>
2024-02-08 23:20:28 +0530

    commit the changes

--- docker-install.sh
+++ docker-install.sh
@@ -1,47 +1,52 @@
 #!/bin/bash
 
+# Function to check if Docker is installed
+checkDockerInstalled() {
+    if ! command -v docker &>/dev/null; then
+        echo "Docker is not installed."
+        return 1
+    else
+        echo "Docker is already installed."
+        return 0
+    fi
+}
 
 # Function to install Docker
 installDocker() {
+    if ! checkDockerInstalled; then
+        echo "##############################################################################################################################"
+        echo "Docker is an open platform for developing, shipping, and running applications."
+        echo "It enables you to separate your applications from your infrastructure so you can deliver software quickly."
+        echo ""
+        echo "Please select your operating system/distribution to install Docker:"
+        echo "##############################################################################################################################"
+        echo ""
 
-    # Print system information
-    echo "##############################################################################################################################"
-    echo "From some basic information on your system, you appear to be running: "
-    echo "    --  OS Name:        $(lsb_release -i)"
-    echo "    --  Description:    $(lsb_release -d)"
-    echo "    --  OS Version:     $(lsb_release -r)"
-    echo "    --  Code Name:      $(lsb_release -c)"
-    echo ""
-    echo "##############################################################################################################################"
-    echo "Docker is an open platform for developing, shipping, and running applications."
-    echo "It enables you to separate your applications from your infrastructure so you can deliver software quickly."
-    echo ""
-    echo "Please select your operating system/distribution to install Docker:"
-    echo ""
-
-    PS3="Select your OS/Distribution: "
-    select os_option in \
-        "Ubuntu" \
-        "Debian" \
-        "CentOS" \
-        "Fedora" \
-        "Amazon Linux" \
-        "Arch Linux" \
-        "OpenSUSE" \
-        "Quit"
-    do
-        case $REPLY in
-            1) installDockerUbuntu ;;
-            2) installDockerDebian ;;
-            3) installDockerCentOS ;;
-            4) installDockerFedora ;;
-            5) installDockerAmazonLinux ;;
-            6) installDockerArchLinux ;;
-            7) installDockerOpenSUSE ;;
-            8) exit ;;
-            *) echo "Invalid selection, please try again..." ;;
-        esac
-    done
+        PS3="Select your OS/Distribution: "
+        select os_option in \
+            "Ubuntu 20.04 or 22.04" \
+            "Debian" \
+            "CentOS" \
+            "Fedora" \
+            "Amazon Linux" \
+            "OpenSUSE" \
+            "Quit"
+        do
+            case $REPLY in
+                1) installDockerUbuntu ;;
+                2) installDockerDebian ;;
+                3) installDockerCentOS ;;
+                4) installDockerFedora ;;
+                5) installDockerAmazonLinux ;;
+                6) installDockerOpenSUSE ;;
+                7) exit ;;
+                *) echo "Invalid selection, please try again..." ;;
+            esac
+        done
+    else
+        echo "Exiting script as Docker is already installed."
+        exit 0
+    fi
 }
 
 # Function to install Docker on Ubuntu
@@ -69,6 +74,8 @@ installDockerCentOS() {
     clear
     echo "Installing Docker on CentOS..."
     sudo yum install -y docker
+    sudo systemctl start docker
+    sudo systemctl enable docker        
     echo "Docker installed successfully!"
     exit 0
 }
@@ -78,6 +85,8 @@ installDockerFedora() {
     clear
     echo "Installing Docker on Fedora..."
     sudo dnf install -y docker
+    sudo systemctl start docker
+    sudo systemctl enable docker
     echo "Docker installed successfully!"
     exit 0
 }
@@ -87,15 +96,8 @@ installDockerAmazonLinux() {
     clear
     echo "Installing Docker on Amazon Linux..."
     sudo yum install -y docker
-    echo "Docker installed successfully!"
-    exit 0
-}
-
-# Function to install Docker on Arch Linux
-installDockerArchLinux() {
-    clear
-    echo "Installing Docker on Arch Linux..."
-    sudo pacman -Sy --noconfirm docker
+    sudo systemctl start docker
+    sudo systemctl enable docker    
     echo "Docker installed successfully!"
     exit 0
 }
@@ -105,6 +107,8 @@ installDockerOpenSUSE() {
     clear
     echo "Installing Docker on OpenSUSE..."
     sudo zypper install -y docker
+    sudo systemctl start docker
+    sudo systemctl enable docker    
     echo "Docker installed successfully!"
     exit 0
 }
